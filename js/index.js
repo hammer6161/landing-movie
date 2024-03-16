b83d5e7fef2b94320289f2b01cc6a00d9597de95
Igor Krylkov <hammer61@mail.ru>
2024-03-16 20:40:09 +0300

    завершение js

--- js/index.js
+++ js/index.js
@@ -21,6 +21,65 @@ createListTrailers = (parrent, srcList) => {
 		trailersVideo.dataset.src = src
 		trailersWrapper.append(trailersVideo)
 		trailerFrames.push(trailersVideo)
+
+		const idVideo = src.match(/\/embed\/([^/\?]+)/)[1]
+		console.log('idVideo: ', idVideo)
+
+		trailersVideo.srcdoc = `
+<style>
+						* {
+			padding: 0;
+			margin: 0;
+			overflow: hidden;
+		}
+
+		html,
+		body {
+			width: 100%;
+			height: 100%;
+		}
+
+		img, svg {
+			position: absolute;
+			top: 0;
+			left: 0;
+			width: 100%;
+			height: 100%;
+		}
+
+		#button {
+			position: absolute;
+			top: 50%;
+			left: 50%;
+			transform: translate(-50%, -50%);
+			z-index: 5;
+			width: 64px;
+			height: 64px;
+			border: none;
+			background-color: transparent;
+			cursor: pointer;
+		}
+
+		@media (max-width: 900px) {
+			#button {
+				width: 36px;
+				height: 36px;
+			}
+		}
+
+</style>
+<a href="https://www.youtube.com/embed/${idVideo}?autoplay=1">
+	<img src="https://img.youtube.com/vi/${idVideo}/maxresdefault.jpg">
+	<div id="button">
+		<svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<circle cx="32" cy="32" r="32" fill="#FF3D00"/>
+			<path d="M42.5 31.134C43.1667 31.5189 43.1667 32.4811 42.5 32.866L27.5 41.5263C26.8333 41.9112 26 41.4301 26 40.6603V23.3397C26 22.5699 26.8333 22.0888 27.5 22.4737L42.5 31.134Z" fill="white"/>
+		</svg>
+	</div>
+</a>
+
+
+		`
 	})
 
 	return { trailerWrappers, trailerFrames }
